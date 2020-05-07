314f3088a6842ca8679b7da20fbfda36f89ae61a
ngmeijer <meijernils9@gmail.com>
2020-05-07 16:02:32 +0200

    Implemented pause menu visuals

--- GXPEngineShare/GXPEngine/HUD.cs
+++ GXPEngineShare/GXPEngine/HUD.cs
@@ -7,11 +7,18 @@ public class HUD : GameObject
     public Sprite _leftButton;
     public Sprite _lowerButton;
 
+    public Sprite _optionsButton;
+    public Sprite _optionsBackground;
+    public Sprite _continueButton;
+    public Sprite _mileStonesButton;
+    public Sprite _exitButton;
+
     public bool clickedBack { get; set; }
     public bool clickedLeft { get; set; }
     public bool clickedRight { get; set; }
-
     public bool mainAreaActive { get; set; }
+    public bool clickedOptions { get; set; }
+
 
     public bool showHUD { get; set; }
 
@@ -31,14 +38,44 @@ public class HUD : GameObject
         AddChild(_lowerButton);
         _lowerButton.x = 560;
         _lowerButton.y = 966;
+
+        _optionsButton = new Sprite("OptionsButton.png");
+        AddChild(_optionsButton);
+        _optionsButton.x = game.width - 100;
+        _optionsButton.y = 20;
+
+        _optionsBackground = new Sprite("OptionsBackground.png");
+        AddChild(_optionsBackground);
+        _optionsBackground.x = game.width / 2 - _optionsBackground.width / 2;
+        _optionsBackground.y = game.height / 2 - _optionsBackground.height / 2;
+        _optionsBackground.visible = false;
+
+        _continueButton = new Sprite("ContinueButton.png");
+        AddChild(_continueButton);
+        _continueButton.x = game.width / 2 - _continueButton.width / 2;
+        _continueButton.y = game.height / 2 - _continueButton.height / 2 - 120;
+        _continueButton.visible = false;
+
+        _mileStonesButton = new Sprite("MileStonesButton.png");
+        AddChild(_mileStonesButton);
+        _mileStonesButton.x = game.width / 2 - _mileStonesButton.width / 2;
+        _mileStonesButton.y = game.height / 2 - _mileStonesButton.height / 2;
+        _mileStonesButton.visible = false;
+
+        _exitButton = new Sprite("ExitButton.png");
+        AddChild(_exitButton);
+        _exitButton.x = game.width / 2 - _exitButton.width / 2;
+        _exitButton.y = game.height / 2 - _exitButton.height / 2 + 120;
+        _exitButton.visible = false;
     }
 
     private void Update()
     {
-        CheckForBackRequest();
+        CheckForAnimalSwitching();
+        CheckForOptionsRequest();
     }
 
-    private void CheckForBackRequest()
+    private void CheckForAnimalSwitching()
     {
         if (_lowerButton.HitTestPoint(Input.mouseX, Input.mouseY))
         {
@@ -63,6 +100,39 @@ public class HUD : GameObject
             {
                 clickedRight = true;
             }
+        }
+    }
+
+    public void CheckForOptionsRequest()
+    {
+        if (_optionsButton.HitTestPoint(Input.mouseX, Input.mouseY) && !clickedOptions)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                clickedOptions = true;
+            }
+        }
+        else if (clickedOptions && !_optionsBackground.HitTestPoint(Input.mouseX, Input.mouseY))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                clickedOptions = false;
+            }
+        }
+
+        if (clickedOptions)
+        {
+            _optionsBackground.visible = true;
+            _continueButton.visible = true;
+            _mileStonesButton.visible = true;
+            _exitButton.visible = true;
+        }
+        else if (!clickedOptions)
+        {
+            _optionsBackground.visible = false;
+            _continueButton.visible = false;
+            _mileStonesButton.visible = false;
+            _exitButton.visible = false;
         }
     }
 }
